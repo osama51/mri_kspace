55e104de7526a62c3bf821baa0e8e6dde84c8111
Mohamed Osama <36142935+osama51@users.noreply.github.com>
2023-06-20 09:50:50 +0300

    saved previous mag state, testing with decay matrices, planning to assign different decay eqs for the different tissues, if you see this before 21/6/2023 and think this is wrong, contact me asap

--- kspace.py
+++ kspace.py
@@ -5,6 +5,12 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import scipy.fftpack as spfft
 
+class Temp: 
+    m16 = np.ndarray((16,16,3))
+    m32 = np.ndarray((32,32,3))
+    m64 = np.ndarray((64,64,3))
+    temp_counter = 0
+    
 class KSpace:
     def __init__(self, phantom):
         super(KSpace,self).__init__()
@@ -28,6 +34,14 @@ class KSpace:
         
         # self.k_space = np.ones((self.num_of_rows, self.num_of_cols), dtype=np.complex64)
         self.real_kspace = np.fft.fft2(self.phantom)
+        
+        # in ms
+        self.T1 = 400
+        self.T2 = 40
+        self.TR = 2000
+        self.TE = 15
+        
+        # self.m = self.magnetization_vector(self.phantom)
     
 
     # Define rotation matrices
@@ -44,36 +58,28 @@ class KSpace:
     def decay(self, TE, TR, T1, T2):
         return np.array([[np.exp(-TE / T2), 0, 0],
                          [0, np.exp(-TE / T2), 0],
-                         [0, 0, np.exp(-TR / T1)]])
+                         [0, 0, 1-np.exp(-TR / T1)]])
+    
+    def decay_mag(self, m, decay):
+        m_reshaped = m.reshape((-1, 3))
+        m_decayed = np.dot(decay, m_reshaped.T).T
+        m = m_decayed.reshape((self.num_of_rows, self.num_of_cols, 3))
+        return m
 
     
-    def RF_pulse(self):
-        # RF
-        # Compute magnetization vectors for all pixels
-        mx = np.cos(np.pi/2) * self.phantom
-        my = np.sin(np.pi/2) * self.phantom
-        # mz = np.zeros_like(self.phantom)
-        mz = np.cos(np.pi/2) * self.phantom
-        m = np.stack([mx, my, mz], axis=-1)
-        
+    def RF_pulse(self, m, theta):
+        m_reshaped = m.reshape((-1, 3))
+        m_rotated_rf = np.dot(self.Rx(theta), m_reshaped.T).T
+        m = m_rotated_rf.reshape((self.num_of_rows, self.num_of_cols, 3))
         return m
     
-    def RF_pulse_rotation(self, theta):
-        mx = np.zeros_like(self.phantom)
-        my = np.zeros_like(self.phantom)
-        mz = self.phantom
-        m_ = np.stack([mx, my, mz], axis=-1)
-        m_rf = m_.copy()
-        
-        for col in range(self.num_of_cols):
-            for row in range(self.num_of_rows):
-                m_rf[row, col, :] = np.dot(self.Rx(theta),m_rf[row, col, :])
-                # m_rf[row, col, :] = np.dot(self.Rz(np.pi/4), m_rf[row, col, :])
-                
-        
-        return m_rf
-    
-    
+    def magnetization_vector(self, phantom):
+        mx = np.zeros_like(phantom)
+        my = np.zeros_like(phantom)
+        # mz = np.zeros_like(self.phantom)
+        mz = phantom
+        m = np.stack([mx, my, mz], axis=-1)
+        return m
     
     """
     For each point in k-space, we will change the phase of all vector pixels
@@ -93,10 +99,28 @@ class KSpace:
     def build_kspace(self, counter, kspace):
         self.num_of_rows = self.phantom.shape[0]
         self.num_of_cols = self.phantom.shape[1]
-        
+
+
+
         # y and x are iterators in K-Space
         for y in range(counter, counter + 1):
-            m = self.RF_pulse()
+            if(counter == 0):
+                m = self.magnetization_vector(self.phantom)
+            else:
+                if(self.num_of_cols==16):
+                    m = Temp.m16
+                elif(self.num_of_cols==32):
+                    m = Temp.m32
+                elif(self.num_of_cols==64):
+                    m = Temp.m64
+                    
+            """ comment the next line when you use decay """
+            m = self.magnetization_vector(self.phantom)
+            # m = self.decay_mag(m, self.decay(self.TE, self.TR, self.T1, self.T2))
+            m = self.RF_pulse(m, np.pi/4)
+            
+
+
             # m = self.RF_pulse_rotation(np.pi/2)
             m_rot = m.copy()
             m_rotx = m.copy()
@@ -113,14 +137,35 @@ class KSpace:
                 # add both angles
                 # thread
                 # histo bar
-                # 
-                for col in range(self.num_of_cols):
-                    for row in range(self.num_of_rows):
-                        # Phase Encoding 
-                        m_rot[row, col, :] = np.dot(self.Rz(Gy[row, col]), m[row, col, :])
+                
+                
+                for row in range(self.num_of_rows):
+                  
+                   for col in range(self.num_of_cols):       
+                       # Phase Encoding 
+                       m_rot[row, col, :] = np.dot(self.Rz(Gy[row, col]), m[row, col, :])
+                       
+                # apply decay matrix
+                # m_rot = self.decay_mag(m_rot, self.decay(self.TE, self.TR, self.T1, self.T2))
+                        
+                for row in range(self.num_of_rows):
+                   
+                    for col in range(self.num_of_cols):       
+                        # # Phase Encoding 
+                        # m_rot[row, col, :] = np.dot(self.Rz(Gy[row, col]), m[row, col, :])
+
                         # Frequency Encoding
                         m_rotx[row, col, :] = np.dot(self.Rz(Gx[row, col]), m_rot[row, col, :])
-                        
+                    
+                if(self.num_of_cols==16):
+                    Temp.m16 = m_rotx
+                elif(self.num_of_cols==32):
+                    Temp.m32 = m_rotx
+                elif(self.num_of_cols==64):
+                    Temp.m64 = m_rotx
+                
+                # Temp.temp_counter += 1
+                # print("temp_counter", Temp.temp_counter)
                 x_sum = np.sum(m_rotx[..., 0])
                 y_sum = np.sum(m_rotx[..., 1])
                 kspace[y, x] = complex(y_sum, x_sum)
